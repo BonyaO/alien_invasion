e7007b525ceb62c1268a630c4a8722bd66eb7db7
Bonya Oryn <bonyaoryn@gmail.com>
2024-03-18 04:52:07 +0100

    Add ship speed and prevent ship from moving out of the game window

--- ship.py
+++ ship.py
@@ -7,6 +7,7 @@ class Ship:
     def __init__(self, ai_game):
         """Initialise the ship and set its starting position"""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # Load the ship image and get its rect
@@ -16,16 +17,24 @@ class Ship:
         # start each new ship at the bottom centre of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Store a decimal value for the ship's horizontal position
+        self.x = float(self.rect.x)
+
         # Movement flag
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
         """Update the ship's position based on the movement flag"""
-        if self.moving_right:
-            self.rect.x += 1
-        elif self.moving_left:
-            self.rect.x -= 1
+
+        # Update the ship's x value, not the rect.
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        elif self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        # Update rect object from self.x
+        self.rect.x = self.x
 
     def blitme(self):
         """Draw the ship at its current location"""
